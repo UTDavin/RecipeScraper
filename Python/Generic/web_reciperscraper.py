1a9267013b06dcca0ca0f3b09c27f62a01542506
MY-BAE\Pirat <dsiu@utexas.edu>
2018-04-17 21:15:52 -0500

    improved generic scraper. Added units scraping, refined list item processing

--- Python/Generic/web_reciperscraper.py
+++ Python/Generic/web_reciperscraper.py
@@ -6,12 +6,73 @@ import urllib.request
 import re
 request = ""
 
+_unitDict = [
+                "ounce",
+                "ounces",
+                "oz",
+                "cup",
+                "cups",
+                "pound",
+                "pounds",
+                "lb",
+                "lbs",
+                "pt",
+                "kg",
+                "kilogram",
+                "kilograms",
+                "g",
+                "gram",
+                "grams",
+                "pint",
+                "pints",
+                "gallon",
+                "gallons",
+                "gal",
+                "package",
+                "packages",
+                "pkg",
+                "teaspoon",
+                "teaspoons",
+                "tsp",
+                "tablespoon",
+                "tablespoons",
+                "tbsp",
+                "quart",
+                "quarts",
+                "liter",
+                "liters",
+                "ltr",
+                "clove",
+                "cloves",
+                "bunch",
+                "bunches",
+                "container",
+                "containers",
+                "pack",
+                "packs",
+                "bottle",
+                "bottles",
+                "can",
+                "cans",
+                "stick",
+                "sticks"
+            ]
+
 # Note: many websites use the easy-recipes style CSS plug-in (?). Classes begin with "ERS" i.e. ERSIngredients, ERSHeadings
 #Could leverage this for scraping, but may not be a long-term solution
 #Points to consider:
 #Ingredients header is usually a single tag consistenting of the string "Ingredients", but NOT ALWAYS
 #Often times, the ingredients are encapsulated within tags of classes containing the word "ingredient(s)" - may be more robust than string search
 # Ingredient items are usually encapsulated within <li> tags, OR within <span> tags WITHIN <li> tags
+
+def __processLiString__(soupli):
+    if soupli.string:
+        return soupli.string
+    fullString = ""
+    for item in soupli.find_all():
+        if item.string:
+            fullString = fullString + " " + item.string
+    return fullString.strip()
 
 def __ERSScrape__(souparg):
     #Common case 1: EasyRecipe plugin
@@ -23,9 +84,10 @@ def __ERSScrape__(souparg):
         print("ERS plugin detected")
         ingredientslist = []
         for ingr in ingredients:
-            if ingr.string:
-                ingredientslist.append(ingr.string)
-                print(__containsQuantity__(ingr.string))
+            ingredient = __processLiString__(ingr)
+            if ingredient:
+                ingredientslist.append(ingredient)
+                print(__containsQuantity__(ingredient))
         return ingredientslist
 
 def __ingrCSSScrape__(souparg): 
@@ -41,46 +103,77 @@ def __ingrCSSScrape__(souparg):
             print(ingrClass)    
             ingredients = []
             for ingr in ingrClass:
-                if ingr.string:
-                    ingredients.append(ingr.string)
-                    print(__containsQuantity__(ingr.string))
+                ingredient = __processLiString__(ingr)
+                if ingredient:
+                    ingredients.append(ingredient)
+                    print(__containsQuantity__(ingredient))
             return ingredients
 
 def __generalScrape__(souparg):
-
     #general case: need to verify list as ingredients list
     generalRecipe = souparg.find_all(['ol', 'ul'])
     print("generic scraping")
+    fullList = []
     for listCandidate in generalRecipe:
         listItems = listCandidate.find_all("li")
-        num_good_items = 0
+        num_quantity_items = 0
+        num_unit_items = 0
         filteredList = []
         for item in listItems:
-            if item.string:
-                filteredList.append(item.string)
-                if len(item.string) > 100: #let's just say an enumerated ingredient string should not exceed 100 chars
+            ingredient = __processLiString__(item)
+            if ingredient:
+                filteredList.append(ingredient)
+                if len(ingredient) > 100: #let's just say an enumerated ingredient string should not exceed 100 chars
                     break
-                if __containsQuantity__(item.string):
-                    num_good_items += 1
-        if num_good_items > len(listItems)/2: #looks like a good list because over half of the list items had a quantity in them
-            return filteredList
-    print("No good ingredients list candidates found")
-    return []
+                if __containsQuantity__(ingredient):
+                    num_quantity_items += 1
+                if __containsUnit__(ingredient):
+                    num_unit_items += 1
+        if num_quantity_items > len(listItems)/2 and num_unit_items > len(listItems)/2: #looks like a good list because over half of the list items had a quantity in them and/or had a unit
+            fullList.extend(filteredList)
+    if len(fullList) == 0:
+        print("No good ingredients list candidates found")
+        return []
+    else:
+        return fullList
+
+def __containsUnit__(inputString):
+    #TODO: reformat decimal_quantity to include 2nd capture group containing a unit (i.e. 'g' in 100g) and add to this method
+    p = re.compile(r'\W+')
+    tokens = p.split(inputString)
+    found = False
+    foundUnits = []
+    for token in tokens:
+        if token.lower() in _unitDict:
+            found = True
+            foundUnits.append(token.lower())    
+    if found:
+        print("found %s in '%s'" % (foundUnits, inputString))
+    return found         
 
 def __containsQuantity__(inputString):
-    #quantifier = re.search('\d+(?=[a-zA-Z]\s)', inputString) # basic quantifier: [number] + whitespace
-    decimal_quantifier = re.search('\d+\.*\d*(?=[a-zA-Z]*\s)', inputString) # basic decimal (optional .) quantifier: [decimal number][optional unit] + whitespace
-    fractional_quantifier = re.search('\d*\s*\d+\/\d+(?=\s)', inputString) # basic fractional quantifier: [fractional] + whitespace
-    #if quantifier:
+    #quantity = re.search('\d+(?=[a-zA-Z]\s)', inputString) # basic quantifier: [number] + whitespace
+    decimal_quantity = re.search('\d+\.*\d*(?=[a-zA-Z]*\s)', inputString) # basic decimal (optional .) quantifier: [decimal number][optional unit] + whitespace
+                                                                            # i.e.
+                                                                            # 1 cup, 1g
+                                                                            # 1.5 cups, 1.5g
+    fractional_quantity = re.search('\d*\s*\d+\/\d+(?=\s)', inputString)  # basic fractional quantifier: [fractional] + whitespace. 1/3 cup, 1 1/3 cup
+                                                                            # i.e.
+                                                                            # 1/3 cup
+                                                                            # 1 1/3 cup
+    #decimal_range_quantity  1.2-1.5 lb
+    #decimal romanized quantity 1 to 1.5 ?
+    #fractional_range_quantity 1/3-2/3 cup
+    #compound fractional quantity 1 and 1/2 cup
+
+    #if quantity:
     #    print("found quantity match")
-    #    print(quantifier.group(0))
-    if decimal_quantifier:
-        print("found decimal quantity match")
-        print(decimal_quantifier.group(0))
-    if fractional_quantifier:
-        print("found fractional quantity match")
-        print(fractional_quantifier.group(0).strip())
-    if decimal_quantifier or fractional_quantifier:
+    #    print(quantity.group(0))
+    if decimal_quantity:
+        print("found decimal quantity match '%s' in '%s'" % (decimal_quantity.group(0), inputString))
+    if fractional_quantity:
+        print("found fractional quantity match '%s' in '%s'" % (fractional_quantity.group(0).strip(), inputString))
+    if decimal_quantity or fractional_quantity:
         return True
     return False
 
@@ -95,6 +188,18 @@ while request != "q":
         quit()
     responsetext = response.read()
     soup = BeautifulSoup(responsetext, "html.parser")
-    print(__ERSScrape__(soup))
-    print(__ingrCSSScrape__(soup))
-    print(__generalScrape__(soup))
+    list1 = __ERSScrape__(soup)
+    list2 = __ingrCSSScrape__(soup)
+    list3 = __generalScrape__(soup)
+    print("Results of EasyRecipe scrape: ")
+    if len(list1) > 0:
+        for item in list1:
+            print(item)
+    print("Results of CSS ingredients class scrape: ")
+    if len(list2) > 0:
+        for item in list2:
+            print(item)
+    print("Results of Generic scrape: ")
+    if len(list3) > 0:
+        for item in list3:
+            print(item)
